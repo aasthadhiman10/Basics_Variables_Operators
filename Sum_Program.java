f0fa3708246581b022e4532d294da83aa8117bad
Aastha Dhiman <aasthadhimann@gmail.com>
2026-06-26 17:23:20 +0530

    Sum_Program.java

---
+++ Sum_Program.java
@@ -0,0 +1,17 @@
+//Problem : Take two variables 'a', 'b' and print their sum;
+//
+//
+//
+//Solution:
+import java.util.*;
+public class Sum_Program {
+    public static void main(String[]args) {
+    Scanner sc = new Scanner (System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.print(sum);
+
+    }
+}
+    
