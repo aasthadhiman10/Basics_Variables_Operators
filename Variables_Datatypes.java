2d0e34f15e8f6b9684b3157c0b1beb50027f6a32
Aastha Dhiman <aasthadhimann@gmail.com>
2026-06-26 17:35:03 +0530

    Add practice code for Java primitive datatypes and multipliers

---
+++ Variables_Datatypes.java
@@ -0,0 +1,11 @@
+public class Variables_Datatypes {
+    public static void main(String[]args) {
+        //variables
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        //System.out.print\n(sum);
+        int mul = a * b;
+        System.out.print(mul);
+  }
+}
